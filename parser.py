a1b4d73db3e10862e765b8571caf1d2aa4bce7f7
Tiziozero <kleidiv123@proton.me>
2025-02-05 14:45:14 +0000

    i finally udnerstand this shit

--- parser.py
+++ parser.py
@@ -1,18 +1,29 @@
+
 class Token:
     # Let's define our basic token types
     INTEGER = 'INTEGER'
     VALUE = 'VALUE'
+    OPERATOR = "OPERATOR"
     IDENTIFIER = 'IDENTIFIER'
     ARROW = 'ARROW'      # for ->
     COLON = 'COLON'      # for :
     INDENT = 'INDENT'    # for tracking indentation
     EOF = 'EOF'          # end of file
 
+    INSTRUCTIONS = [
+        "PUSH" , "POP" , "SET" , "MOV" , "LDA" , "STA",
+        "MOD" , "ADD" , "SUB" , "MLT" , "DV", "JMP" , "CMP"
+    ]
+
     def __init__(self, type, value=None):
         self.type = type
         self.value = value
 
     def __str__(self):
+        if self.value:
+            return f"Type: {self.type} Value:({self.value})"
+        return str(self.type)
+    def __repr__(self):
         if self.value:
             return f"{self.type}({self.value})"
         return str(self.type)
@@ -21,11 +32,11 @@ class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        self.current_char = self.text[self.pos] if text else None
+        self.current_char:str | None = self.text[self.pos] if text else None
         self.current_indent = 0  # Track indentation level
     
     def error(self):
-        raise Exception(f'Invalid character {self.current_char}')
+        raise Exception(f'Invalid character {self.current_char}, at {self.pos}')
     
     def advance(self):
         """Move to next character"""
@@ -38,10 +49,17 @@ class Lexer:
     def skip_whitespace(self):
         """Skip spaces and tabs"""
         while self.current_char and self.current_char.isspace():
+    def skip_comment(self):
+        """Skip spaces and tabs"""
+        while self.current_char and self.current_char is not "\n":
             self.advance()
 
     def get_next_token(self) :
         while self.current_char is not None: # while file not finished
+            if not self.current_char:
+                raise Exception("Failed to get next char/token")
+            if self.current_char == "-" and self.peek() == "-":
+                self.skip_comment()
             # skip whitespaces
             if self.current_char.isspace():
                 self.skip_whitespace()
@@ -59,13 +77,37 @@ class Lexer:
             if self.current_char == ':':
                 self.advance()
                 return Token(Token.COLON)
+            
+            if self.current_char.isnumeric():
+                return Token(Token.IDENTIFIER, self.get_number())
+                ...
             self.error()
         return Token(Token.EOF)
+    
     def peek(self):
         pos = self.pos + 1
         if pos > len(self.text):
             return None
         return self.text[pos]
+
+    def  get_number(self):
+        res = ''
+        while self.current_char :
+            if self.current_char.isnumeric():
+                res += self.current_char
+                self.advance()
+                continue
+            elif self.current_char == "e" and "e" not in res:
+                res += self.current_char
+                self.advance()
+                continue
+            elif self.current_char == "." and "." not in res:
+                res += self.current_char
+                self.advance()
+                continue
+            break
+        return res
+
     def  get_identifier(self):
         res = ''
         while self.current_char and self.current_char.isalnum():
@@ -76,25 +118,82 @@ class Lexer:
 class Parser:
     def __init__(self, lexer: Lexer) -> None:
         self.lexer: Lexer = lexer
-        self.token_index = 0
-
-    def program(self):
-        # expect IDENTIFIER + COLON | ARROW
+        self.tokens = self.tokenize()  # Store all tokens
+        # print(self.tokens, self.tokens[0])
+        self.token_index = 0  # Current position in the token list
+    
+    def tokenize(self):
+        tokens = []
         while True:
             token = self.lexer.get_next_token()
+            tokens.append(token)
+            if token.type == Token.EOF:
+                break
+        print(tokens)
+        return tokens
+
+
+    def eat(self, expected_type):
+        """Consume a token if it matches the expected type."""
+        if self.tokens[self.token_index].type == expected_type:
             self.token_index += 1
-            if token.type != Token.IDENTIFIER:
-                self.error(f"expected identifier, found {token}")
+        else:
+            self.error(f"Expected {expected_type}, got {self.tokens[self.token_index]}")
+    def program(self):
+        """Parse a program, which consists of operations and procedures."""
+        operations = []
+        while self.tokens[self.token_index].type != Token.EOF:
+            operations.append(self.operation())
+        return operations
+    def operation(self):
+        """Parse an operation, which can be an instruction or an identifier definition."""
+        token = self.tokens[self.token_index]
 
-            token = self.lexer.get_next_token()
-            # if it's a colon then it should be a variable
-            if token.type == Token.COLON:
-                value_token = self.lexer.get_next_token()
-                if value_token.type != Token.VALUE:
-                    self.error(f"expected value, found {value_token.type}")
-                
+        if token.type == Token.IDENTIFIER:
+            # Identifier definition (e.g., "label: push 3")
+            self.eat(Token.IDENTIFIER)
 
-            
+            if self.tokens[self.token_index].type == Token.COLON:
+                self.eat(Token.COLON)
+                instr_token = self.tokens[self.token_index]
+                if instr_token.value in Token.INSTRUCTIONS:
+                    return self.instruction()
+                else:
+                    self.error(f"Invalid instruction after identifier: {instr_token}")
+
+        elif token.value in Token.INSTRUCTIONS:
+            # Direct instruction
+            return self.instruction()
+
+        self.error(f"Unexpected token: {token}")
+
+    def instruction(self):
+        """Parse an instruction and its arguments."""
+        instr_token = self.tokens[self.token_index]
+        self.eat(Token.IDENTIFIER)  # Consume instruction
+
+        if self.tokens[self.token_index].type in [Token.INTEGER, Token.IDENTIFIER]:
+            return (instr_token.value, self.expression())  # Support expressions
+
+        return (instr_token.value,)
+
+
+    def expression(self):
+        """Parse an expression (a single value or a math operation)."""
+        left = self.tokens[self.token_index]
+        self.eat(left.type)
+
+        if self.tokens[self.token_index].type == Token.OPERATOR:
+            op = self.tokens[self.token_index]
+            self.eat(Token.OPERATOR)
+            right = self.tokens[self.token_index]
+            self.eat(right.type)
+            return (op.value, left.value, right.value)  # Example: ('+', 'identifier', 3)
+
+        return left.value  # Just a single value
+
+
+
 
 
     def error(self, reason=None):
@@ -107,35 +206,13 @@ class Parser:
 
 def parse(text):
     lexer = Lexer(text)
-    # parser = Parser(lexer)
-    # return parser.program()
+    _ = Parser(lexer)
 
 # Example usage:
 if __name__ == "__main__":
-    lexer = Lexer("main -> int:")
-    token = lexer.get_next_token()  # should get IDENTIFIER(main)
-    print(token)
-    token = lexer.get_next_token()  # should get ARROW
-    print(token)
-    token = lexer.get_next_token()  # should get IDENTIFIER(int)
-    print(token)
-    token = lexer.get_next_token()  # should get COLON
-    print(token)
-    token = lexer.get_next_token()  # should get EOF
-    print(token)
-    exit()
-    sample = """
-    main -> int:
-        var1: push 3    -- push to stack and save reference
-        var2: push 5    -- push to stack
-        pop             -- pop var2
-        return var1     -- return var1
-    
-    print_char -> char:
-        push "A"
-        return
-    """
-    
-    ast = parse(sample)
-    import json
-    print(json.dumps(ast, indent=2))
+    sample = """push 3 push 4 add """
+    with open("testfile") as f:
+        test = f.read();
+        ast = parse(test)
+    # import json
+    # print(json.dumps(ast, indent=2))
